de83197e29ca6bae21cebee268180fad8ad6065f
youngmi97 <jeong7331@kaist.ac.kr>
2020-09-23 16:32:48 +0900

    express setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -14,11 +14,21 @@ class App extends Component {
       .then((res) => this.setState({ apiResponse: res }));
   }
 
-  componentWillMount() {
+  componentDidMount() {
     this.callAPI();
   }
 
-  render() {}
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title"> Welcome to React</h1>
+        </header>
+        <p className="App-intro">{this.state.apiResponse}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
