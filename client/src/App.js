0503d524db99a96755efefc8f40bfcd514cc6fab
youngmi97 <jeong7331@kaist.ac.kr>
2020-09-23 15:58:11 +0900

    add client

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then((res) => res.text())
+      .then((res) => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
+  render() {}
+}
+
+export default App;
