5cad058019ddb13a189538ae428df78688f64a12
youngmi97 <jeong7331@kaist.ac.kr>
2020-10-11 22:54:44 +0900

    auth logic update

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,39 +9,47 @@ import { FETCH_POSTS_QUERY } from "../util/graphql";
 
 function Home() {
   const { user } = useContext(AuthContext);
-  const { loading, error, data } = useQuery(FETCH_POSTS_QUERY);
+  //   const { loading, error, data } = useQuery(FETCH_POSTS_QUERY);
 
-  if (error) {
-    console.log(`Error! ${error.message}`);
-  }
-  console.log("posts data", data);
-  const posts = data.getPosts;
+  //   if (error) {
+  //     console.log(`Error! ${error.message}`);
+  //   }
+  //   console.log("posts data", data);
+  //   const posts = data.getPosts;
+  console.log("user info", user);
+
+  const display = user ? (
+    <div>You have been logged in!</div>
+  ) : (
+    <div>Landing Page Goes Here</div>
+  );
 
   return (
-    <Grid columns={3}>
-      <Grid.Row className="page-title">
-        <h1>Recent Posts</h1>
-      </Grid.Row>
-      <Grid.Row>
-        {user && (
-          <Grid.Column>
-            <PostForm />
-          </Grid.Column>
-        )}
-        {loading ? (
-          <h1>Loading posts..</h1>
-        ) : (
-          <Transition.Group>
-            {posts &&
-              posts.map((post) => (
-                <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
-                  <PostCard post={post} />
-                </Grid.Column>
-              ))}
-          </Transition.Group>
-        )}
-      </Grid.Row>
-    </Grid>
+    // <Grid columns={3}>
+    //   <Grid.Row className="page-title">
+    //     <h1>Recent Posts</h1>
+    //   </Grid.Row>
+    //   <Grid.Row>
+    //     {user && (
+    //       <Grid.Column>
+    //         <PostForm />
+    //       </Grid.Column>
+    //     )}
+    //     {loading ? (
+    //       <h1>Loading posts..</h1>
+    //     ) : (
+    //       <Transition.Group>
+    //         {posts &&
+    //           posts.map((post) => (
+    //             <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+    //               <PostCard post={post} />
+    //             </Grid.Column>
+    //           ))}
+    //       </Transition.Group>
+    //     )}
+    //   </Grid.Row>
+    // </Grid>
+    display
   );
 }
 
