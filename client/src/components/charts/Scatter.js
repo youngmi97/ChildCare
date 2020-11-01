4ddbcf5eed2ae56a1aa5a42df69ec74fccfb6fc3
youngmi97 <jeong7331@kaist.ac.kr>
2020-11-01 22:18:16 +0900

    tmp

--- client/src/components/charts/Scatter.js
+++ client/src/components/charts/Scatter.js
@@ -3,47 +3,45 @@ import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
 class Scatter extends Component {
-    render() {
-        const series2 = this.props.data;    //App.js에서 데이터를 보내줄 예정
-        const options = {
-            chart: {
-                type: 'scatter'		// bar차트. 아무 설정이 없으면 line chart가 된다.
-            },
-            title: {
-                text: '기본정보'
-            },
-            credits: {
-                enabled: false
-            },
-            xAxis: {
-                type: 'category',
-            },
-            yAxis:{
-                title: null
-            },
-
-            legend: {
-                enabled: false
-            },
-            tooltip: {
-                enabled:false
-            },
-            plotOptions: {
-                series: {
-                    stacking: 'normal',
-                    dataLabels: {
-                        enabled: true,
-                    },
-                }
-            },
-            series: [{ name: '기본정보' , data: series2 }]
-
-        }
-        return (
-            <Fragment>
-                <HighchartsReact highcharts={Highcharts} options={options} />
-            </Fragment>
-        );
-    }
+  render() {
+    const series2 = this.props.data; //App.js에서 데이터를 보내줄 예정
+    const options = {
+      chart: {
+        type: "scatter", // bar차트. 아무 설정이 없으면 line chart가 된다.
+      },
+      title: {
+        text: "기본정보",
+      },
+      credits: {
+        enabled: false,
+      },
+      xAxis: {
+        type: "category",
+      },
+      yAxis: {
+        title: null,
+      },
+      legend: {
+        enabled: false,
+      },
+      tooltip: {
+        enabled: false,
+      },
+      plotOptions: {
+        series: {
+          stacking: "normal",
+          dataLabels: {
+            enabled: true,
+          },
+        },
+      },
+      series: [{ name: "기본정보", data: series2 }],
+    };
+    return (
+      <Fragment>
+        <HighchartsReact highcharts={Highcharts} options={options} />
+      </Fragment>
+    );
+  }
 }
 export default Scatter;
