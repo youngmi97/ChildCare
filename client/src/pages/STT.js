658d77caef0ba2eec3a7c347f6a79a39ee4193c8
youngmi97 <jeong7331@kaist.ac.kr>
2020-11-24 01:03:56 +0900

    test REST cors

--- client/src/pages/STT.js
+++ client/src/pages/STT.js
@@ -5,6 +5,8 @@ import STTResults from "../components/STTResults";
 import STTUploadVideo from "../components/STTUploadVideo";
 import { AuthContext } from "../context/auth";
 import { Grid } from "@material-ui/core";
+const axios = require("axios");
+
 function STT() {
   const { user } = useContext(AuthContext);
   const [videoFiles, setVideoFiles] = useState([]);
@@ -30,6 +32,21 @@ function STT() {
   useEffect(() => {
     if (videoFiles.length != 0) {
       console.log("videoFile data changed");
+      // test api gateway REST call
+      const api =
+        "https://85sgmxl2m9.execute-api.us-east-1.amazonaws.com/staging2";
+      const data = { name: "Mike" };
+
+      // Something wrong on the lambda side
+
+      axios
+        .post(api, data)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
   }, [videoFiles]);
 
