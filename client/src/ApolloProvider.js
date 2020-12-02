914da11a8965b96fb93be0b73cbd72d18c3c2ff8
youngmi97 <jeong7331@kaist.ac.kr>
2020-12-02 10:06:22 +0900

    test build

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -8,7 +8,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "/graphql",
 });
 
 const authLink = setContext(() => {
--- index.js
+++ index.js
@@ -10,29 +10,48 @@ const { callSttPromise } = require("./util/aws-stt");
 const dotenv = require("dotenv").config({
   path: require("find-config")(".env"),
 });
+const graphqlHTTP = require("express-graphql");
 const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
 
 const pubsub = new PubSub();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => ({ req, pubsub }),
+    introspection: true,
+    playground: true,
+  })
+);
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ req }) => ({ req, pubsub }),
-  introspection: true,
-  playground: true,
-});
+//const pubsub = new PubSub();
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: ({ req }) => ({ req, pubsub }),
+//   introspection: true,
+//   playground: true,
+// });
 
 MONGODB();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.use(express.static("public"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
-server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
