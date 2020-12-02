71be5a54b8bb7e98e9471ea2d8d9bb4f7ddd5808
youngmi97 <jeong7331@kaist.ac.kr>
2020-12-02 10:29:03 +0900

    refactor to apollo express

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { ApolloServer, PubSub } = require("apollo-server");
+//const { ApolloServer, PubSub } = require("apollo-server");
+const { ApolloServer, PubSub } = require("apollo-server-express");
 const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
@@ -10,7 +11,8 @@ const { callSttPromise } = require("./util/aws-stt");
 const dotenv = require("dotenv").config({
   path: require("find-config")(".env"),
 });
-const graphqlHTTP = require("express-graphql");
+const { graphqlConnect } = require("apollo-server-express");
+const http = require("http");
 const path = require("path");
 
 const PORT = process.env.PORT || 5000;
@@ -19,39 +21,28 @@ const app = express();
 app.use(cors());
 
 const pubsub = new PubSub();
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    typeDefs,
-    resolvers,
-    context: ({ req }) => ({ req, pubsub }),
-    introspection: true,
-    playground: true,
-  })
-);
 
-//const pubsub = new PubSub();
-
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-//   context: ({ req }) => ({ req, pubsub }),
-//   introspection: true,
-//   playground: true,
-// });
-
-MONGODB();
+app.use("/graphql", bodyParser.json());
 
 app.use(express.static("public"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req, pubsub }),
+  introspection: true,
+  playground: true,
+});
+
+server.applyMiddleware({ app });
+
+MONGODB();
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// http
+//   .createServer(app)
+//   .listen(PORT, () => console.log(`Server started on port ${PORT}`));
