d351105a796cdaad6d5b722294b74eb0a41be1ff
youngmi97 <jaehwan@sureplus.io>
2021-04-22 03:21:05 +0900

    refine env vars

--- client/src/pages/STT.js
+++ client/src/pages/STT.js
@@ -96,8 +96,8 @@ function STT() {
     //parse the url and get stt .json
 
     var s3 = new AWS.S3({
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
       region: "us-east-1",
     });
 
